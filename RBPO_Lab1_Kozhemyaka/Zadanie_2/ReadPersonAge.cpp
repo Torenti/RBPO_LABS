3cf4353df2459bc62c62f723ce39c9d99d83d342
Torenti <iivan3840@gmail.com>
2023-09-24 23:56:40 +0300

    LAB1

---
+++ RBPO_Lab1_Kozhemyaka/Zadanie_2/ReadPersonAge.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+unsigned short ReadPersonAge()
+{
+	unsigned short age;
+	cin >> age;
+	return age;
+}
---
+++ RBPO_Lab1_Kozhemyaka/Zadanie_2/ReadPersonHeight.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+unsigned short height;
+
+void ReadPersonHeight()
+{
+	cin >> height;
+}
